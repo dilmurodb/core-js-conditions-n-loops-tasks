af5f4a9ce4e19f1b61f8c4a30762724a700ae1ff
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-01-19 20:53:46 +0500

    feat: 2nd task complete

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -41,8 +41,13 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  if (a > b && a > c) {
+    return a;
+  } else if (b > a && b > c) {
+    return b;
+  }
+  return c;
 }
 
 /**
